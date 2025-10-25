ed7f70e5ab368a07c89500437a60a608b8103095
Ines Joao De Carvalho Portela <223902517+IJCP2000@users.noreply.github.com>
2025-10-25 15:16:35 +0100

    ft_strtrim +25

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,19 +6,74 @@
 /*   By: ide-carv <ide-carv@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 17:50:10 by ide-carv          #+#    #+#             */
-/*   Updated: 2025/10/23 20:40:27 by ide-carv         ###   ########.fr       */
+/*   Updated: 2025/10/25 15:05:40 by ide-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdlib.h>
 
-char *ft_strtrim(char const *s1, char const *set)
+static int	ft_trim(const char *s, char c);
+
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	while(s1[j] || set[i])
+	size_t	i;
+	size_t	j;
+	size_t	l;
+	char	*trimmed;
+
+	j = ft_strlen(s1) - 1;
+	i = 0;
+	l = 0;
+	if (!s1 || !set)
+		return (NULL);
+	while (ft_trim(set, s1[i]))
+		i++;
+	while (ft_trim(set, s1[j]))
+		j--;
+	if (i > j)
+		return (ft_strdup(""));
+	trimmed = malloc ((j - i + 2) * sizeof(char));
+	if (!trimmed)
+		return (NULL);
+	while (i <= j)
 	{
-		while(set[i] == s1[j])
-			j++;
-		while(set[i] != s1[j] || set[i])
-			i++;
-	
+		trimmed[l] = s1[i];
+		i++;
+		l++;
 	}
+	trimmed[l] = '\0';
+	return (trimmed);
 }
+
+static int	ft_trim(const char *s, char c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i] == c))
+			return (1);
+		i++;
+	}
+	if ((s[i] == c))
+		return (1);
+	return (0);
+}
+/*
+int	main(void)
+{
+	char s1[] = "ababaaaMy name is Simonbbaaabbad";
+	char set[] = "abd";
+
+	printf("BEFORE TRIM\n");
+	printf("%s\n\n", s1);
+
+	char *trimmed = ft_strtrim(s1, set);
+	printf("AFTER TRIM\n");
+	printf("%s\n", trimmed);
+	free (trimmed);
+	trimmed = NULL;
+	return (0);
+}
+*/
